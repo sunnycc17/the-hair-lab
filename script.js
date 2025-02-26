47414780b148db24d92b31bc1349bc38131be173
sunny <alkristy7@gmail.com>
2025-02-26 17:08:26 +0200

    added lightbox2 for gallery images

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  lightbox.option({
+    fadeDuration: 150,
+    resizeDuration: 100,
+    imageFadeDuration: 150,
+    wrapAround: true,
+    albumLabel: "Image %1 of %2",
+  });
+});
